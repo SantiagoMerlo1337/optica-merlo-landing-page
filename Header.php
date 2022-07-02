f35df9798bf2d428500cf33f1c5537da2ad4c398
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2022-07-02 02:26:44 -0300

    new

---
+++ Header.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg px-md-5">
+    <div class="container-fluid">
+      <a href="#"><img class="navbar-brand brandImagen" src="./img/logo-desc-unalinea.png"></a>
+        <button class="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="container-fluid d-flex justify-content-between p py-2">
+              <ul class="navbar-nav gap-lg-4 ps-lg-5">
+                  <li class="nav-item">
+                    <a class="nav-link active px-3 fw-bold" aria-current="page" href="#">Inicio</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link px-3 fw-bold" href="marcas.html">Marcas</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link px-3 fw-bold" href="filtros.html">Filtros</a>
+                  </li>
+                </ul>
+                <a class="btn btn-primary d-flex align-items-center contactoHeader" href="contacto.html">CONTACTANOS</a>
+            </div>
+        </div>
+    </div>
+</nav> 
