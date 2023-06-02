3532db2f5d01d3a961a2dac25a0288ebe4c9a134
Santiago Merlo <66649631+SantiagoMerlo1337@users.noreply.github.com>
2023-06-02 11:45:41 -0300

    Add Carousel

--- src/pages/Filtros.jsx
+++ src/pages/Filtros.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Header from '../components/Header/Header.jsx'
 import TextBand from '../components/TextBand/TextBand.jsx'
+import Carousel from '../components/Carousel/Carousel.jsx'
+import CarouselRB from 'react-bootstrap/Carousel'
+
+//Assets
+import filtrosJson from '../assets/data/filtros.json'
 
 export default function Filtros() {
   return (
@@ -8,7 +13,12 @@ export default function Filtros() {
       <Header title='Filtros disponibles'/>
       <TextBand text='¡Visitanos a nuestro local con tu receta para adquirirlos!'/>
       <main>
-        
+        <CarouselRB>
+          {filtrosJson.map((item, index) => {
+            console.log(item); 
+            return <Carousel key={index} item={item} />;
+          })}
+        </CarouselRB>
       </main>
     </>
   )
