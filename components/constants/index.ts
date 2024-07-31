bf2aec63e1e4cf5e13cafe24de0c582f51ea5787
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-07-31 12:08:46 -0300

    add types and constants

---
+++ components/constants/index.ts
@@ -0,0 +1,20 @@
+import { NavbarLink } from "../../types";
+
+export const navbarLinks: NavbarLink[] = [
+    {
+        route: "/",
+        label: "Inicio",
+    },
+    {
+        route: "/community",
+        label: "Marcas",
+    },
+    {
+        route: "/filtros",
+        label: "Filtros",
+    },
+    {
+        route: "/contacto",
+        label: "Contactanos",
+    },
+];
