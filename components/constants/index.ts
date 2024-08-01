993272593f7babe11eaf9b03ee0aad5430b24b84
SantiagoMerlo1337 <santiagomerlo1337@gmail.com>
2024-08-01 18:41:32 -0300

    update globla.css, index.ts, theme.css and types

--- components/constants/index.ts
+++ components/constants/index.ts
@@ -1,4 +1,4 @@
-import { NavbarLink, SocialMedia } from "../../types";
+import { NavbarLink, SocialMedia, Marcas } from "../../types";
 
 export const navbarLinks: NavbarLink[] = [
     {
@@ -6,7 +6,7 @@ export const navbarLinks: NavbarLink[] = [
         label: "Inicio",
     },
     {
-        route: "/community",
+        route: "/marcas",
         label: "Marcas",
     },
     {
@@ -23,16 +23,144 @@ export const socialMedia: SocialMedia[] = [
     {
         url: "/",
         label: "WhatsApp",
-        imgUrl: "/assets/icons/whatsapp.png"
+        imgUrl: "/assets/icons/social-media/whatsapp.png"
     },
     {
         url: "/",
         label: "Instagram",
-        imgUrl: "/assets/icons/instagram.png"
+        imgUrl: "/assets/icons/social-media/instagram.png"
     },
     {
         url: "/",
         label: "Facebook",
-        imgUrl: "/assets/icons/facebook.png"
+        imgUrl: "/assets/icons/social-media/facebook.png"
     },
 ];
+
+export const marcas: Marcas[] = [
+    {
+        "imgUrl": "/assets/images/logos-marcas/why-not.png",
+        "label": "Why Not?",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/reef.png",
+        "label": "Reef",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/vulk.png",
+        "label": "Vulk",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/rusty.png",
+        "label": "Rusty",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/muaa.png",
+        "label": "Muaa",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/mistral.png",
+        "label": "Mistral",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/hang-loose.png",
+        "label": "Hang Loose",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/ay-not-dead.png",
+        "label": "Ay Not Dead",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/e7.png",
+        "label": "E7",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/uptown.png",
+        "label": "Uptown",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/ossira.png",
+        "label": "Ossira",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/alberta-ferretti.png",
+        "label": "Alberta Ferretti",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/las-oreiro.png",
+        "label": "Las Oreiro",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/phancolor.png",
+        "label": "Phancolor",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/prune.png",
+        "label": "Prüne",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/karun.png",
+        "label": "Karün",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/wanama.png",
+        "label": "Wanama",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/ona-saez.png",
+        "label": "Ona Saez",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/prototype.png",
+        "label": "Prototype",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/rodenstock.png",
+        "label": "Rodenstock",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/vondel-park.png",
+        "label": "Vondel Park",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/mormaii.png",
+        "label": "Mormaii",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/mira-flex.png",
+        "label": "Mira Flex",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/union-pacific.png",
+        "label": "Union Pacific",
+        "link": ""
+    },
+    {
+        "imgUrl": "/assets/images/logos-marcas/tiffany.png",
+        "label": "Tiffany",
+        "link": ""
+    }
+]
--- types/index.d.ts
+++ types/index.d.ts
@@ -8,3 +8,9 @@ export interface SocialMedia {
     label: string;
     imgUrl: string;
 }
+
+export interface Marcas {
+    imgUrl: string;
+    label: string;
+    link?: string;
+}
