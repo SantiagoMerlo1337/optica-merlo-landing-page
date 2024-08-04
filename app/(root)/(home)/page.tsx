490c448393b20462a9b8fe5de57994711d4f1607
SantiagoMerlo1337 <santiagomerlo1337@gmail.com>
2024-08-04 20:39:58 -0300

    update home, contacto and filtros page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,7 +8,7 @@ const filtrosHref = navbarLinks[2].route
 const Home = () => {
     return (
         <>
-            <section className="grid grid-cols-1 grid-rows-4 lg:grid-cols-2 lg:grid-rows-2 gap-14 w-full md:w-5/6 lg:w-full ">
+            <section className="grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-2 gap-14 w-full lg:w-full ">
                 <Card title="LENTES DE RECETA" desc="Contamos con filtros de luz azul, antirreflejos, complex, y mucho más." imgAlt="Lentes de receta imagen" imgUrl="/assets/icons/los-anteojos.png" hrefLink="/filtros"/>
                 <Card title="LENTES DE SOL" desc="Contamos con lentes de calidad premium de distintas marcas, armazones de nylon, metálicos, y mucho más." imgAlt="Lentes de sol imagen" imgUrl="/assets/icons/gafas-de-sol.png" hrefLink="/marcas"/>
                 <Card title="LENTES DE CONTACTO" desc="Ofrecemos una amplia variedad de lentes de contacto, incluyendo lentes diarios, mensuales y especializados." imgAlt="Lentes de contacto imagen" imgUrl="/assets/icons/lentes-de-contacto.png" hrefLink="/lentes-de-contacto"/>
